9f4be8a57e645986f17117bffe68259f8af0499d
peerasu <peerasu.w@ku.th>
2021-03-17 19:46:44 +0700

    resolve #3; Show a single pillar pair

--- flappydot.py
+++ flappydot.py
@@ -22,7 +22,9 @@ class Dot(Sprite):
 class FlappyGame(GameApp):
     def create_sprites(self):
         self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
+        self.pillar_pair = PillarPair(self, 'images/pillar-pair.png', CANVAS_WIDTH, CANVAS_HEIGHT // 2)
 
+        self.elements.append(self.pillar_pair)
         self.elements.append(self.dot)
 
     def init_game(self):
@@ -36,6 +38,10 @@ class FlappyGame(GameApp):
 
     def on_key_pressed(self, event):
         pass
+
+
+class PillarPair(Sprite):
+    pass
 
 
 if __name__ == "__main__":
